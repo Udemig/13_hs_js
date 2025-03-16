ce172a05da6ab7bbc4ede93ac7aceedb33f6b04b
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-03-16 22:39:31 +0300

    noteMap note adding,deleting,flight and render marker features

--- 2-)projeler/6-)note-map/js/main.js
+++ 2-)projeler/6-)note-map/js/main.js
@@ -1,4 +1,5 @@
 import { personIcon } from "./constant.js";
+import { getNoteIcon, getStatus } from "./helper.js";
 import elements from "./ui.js";
 
 /*
@@ -10,6 +11,8 @@ import elements from "./ui.js";
 // * Global Variables
 let clickedCoords;
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let layer;
+let map;
 
 console.log(notes);
 
@@ -28,7 +31,7 @@ window.navigator.geolocation.getCurrentPosition(
 
 function loadMap(currentPosition, message) {
   // Haritanın Kapsayıcısının Kurulumu ve default zoom çubuğunu iptal et
-  var map = L.map("map", { zoomControl: false }).setView(currentPosition, 11);
+  map = L.map("map", { zoomControl: false }).setView(currentPosition, 10);
 
   // Harita Content Kurulumu
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -44,8 +47,17 @@ function loadMap(currentPosition, message) {
   // Kullanıcının başlanngıç konumuna bir marker ekle
   L.marker(currentPosition, { icon: personIcon }).addTo(map).bindPopup(message);
 
+  // Harita üzerine bir layer katmanı ekle
+  layer = L.layerGroup().addTo(map);
+
   // Harita üzerindeki tıklanma olayını izle
   map.on("click", onMapClick);
+
+  // Notları render et
+  renderNotes();
+
+  // Markerları render et
+  renderMarkers();
 }
 
 // ! Harita üzerinde tıklanma olayı gerçekleşince çalışacak fonksiyon
@@ -96,26 +108,107 @@ elements.form.addEventListener("submit", (e) => {
   // Aside kısmını eski haline çevir
 
   elements.aside.classList.remove("add");
+
+  // Notları render et
+  renderNotes();
+
+  // Markarları render et
+  renderMarkers();
 });
 
 // ! Mevcut notları aside kısmındaki liste içerisinde renderlayan fonksiyon
-
 function renderNotes() {
   // Notes dizisini dönüp her bir note için bir html oluştur
-  const noteCards = notes.map(
-    (note) => ` <li>
-          <div>
-            <p>Title</p>
-            <p>Date</p>
-            <p>Status</p>
-          </div>
-          <div class="icons">
-            <i class="bi bi-airplane-fill" id="fly-btn"></i>
-            <i class="bi bi-trash" id="delete-btn"></i>
-          </div>
-        </li>`
-  );
-  // Elde edilen Html'i aside kısmındaki liste'ye aktar
+  const noteCards = notes
+    .map((note) => {
+      // Tarih ayarlaması
+      const date = new Date(note.date).toLocaleDateString("tr", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      });
+      return ` <li>
+        <div>
+          <p>${note.title}</p>
+          <p>${date}</p>
+          <p>${getStatus(note.status)}</p>
+        </div>
+        <div class="icons">
+          <i data-id='${note.id}' class="bi bi-airplane-fill" id="fly-btn"></i>
+          <i data-id='${note.id}' class="bi bi-trash" id="delete-btn"></i>
+        </div>
+      </li>`;
+    })
+    .join("");
 
+  // Elde edilen Html'i aside kısmındaki liste'ye aktar
   elements.noteList.innerHTML = noteCards;
+
+  // * Delete Iconlara eriş
+  document.querySelectorAll("#delete-btn").forEach((btn) => {
+    // Silinecek elemanın id'sini sil iconuna atanan id değerinden al ve bunu id değişkenine ata
+    const id = btn.dataset.id;
+    btn.addEventListener("click", () => {
+      // Silme işlemi yapacak fonksiyona silinecek elemanın id'sini ver
+      deleteNote(id);
+    });
+  });
+
+  // *  Fly Iconlarına eriş
+  document.querySelectorAll("#fly-btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      // Uçulacak elemanın id'sini fly iconundan al
+      const id = btn.dataset.id;
+
+      // Elde edilen uçuş iconunu flyToNote fonksiyonuna parametre olarak geç
+      flyToNote(id);
+    });
+  });
 }
+
+// ! Silme işlemi yapan fonksiyon
+function deleteNote(id) {
+  // Kullanıcıdan silme işlemi için onay al
+  const response = confirm("Not silme işlemini onaylıyor musunuz ?");
+
+  // Eğer kullanıcı silme işlemini onayladıysa id'si bilinen notu sil
+  if (response) {
+    // İd'si bilinen notu notes dizisinden kaldır
+    notes = notes.filter((note) => note.id != id);
+
+    // Silme işlemi sonrasında localstorage'ı güncelle
+    localStorage.setItem("notes", JSON.stringify(notes));
+
+    // Notları render et
+    renderNotes();
+
+    // Markerları render et
+    renderMarkers();
+  }
+}
+
+// ! Bir note uçma özelliğini gerçekleştiren fonksiyon
+function flyToNote(id) {
+  // Id'si bilinen elemanı note dizisi içerisinden bul
+  const foundedNote = notes.find((note) => note.id == id);
+
+  // Bulunan elemana uç
+  map.flyTo(foundedNote.coords, 11);
+}
+
+// ! Mevcut notlar için birer marker render eden fonksiyon
+function renderMarkers() {
+  // Haritadaki markerları sıfırla
+  layer.clearLayers();
+  // noteları dön ve herbir note için birer marker render et
+  notes.map((note) => {
+    const icon = getNoteIcon(note.status);
+    // Marker render et
+    L.marker(note.coords, { icon }).addTo(layer);
+  });
+}
+
+// ! Aside kısmına hide mode'a sokan fonksiyon
+elements.arrowIcon.addEventListener("click", () => {
+  elements.aside.classList.toggle("hide");
+});
--- 2-)projeler/6-)note-map/js/ui.js
+++ 2-)projeler/6-)note-map/js/ui.js
@@ -3,6 +3,7 @@ const elements = {
   cancelBtn: document.querySelector("#cancel"),
   form: document.querySelector("form"),
   noteList: document.querySelector("ul"),
+  arrowIcon: document.querySelector("#arrow"),
 };
 
 export default elements;
